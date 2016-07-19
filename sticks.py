6dcfb0d6f4accb9872fef59e6b512230132dc287
sateigen <shannon.teigen@gmail.com>
2016-07-19 12:31:52 -0400

    Working tests and functions for determining if player choice is a number between 1 and 3, determining how many sticks are left in the game, and if the game is over when there is only one stick left.

--- sticks.py
+++ sticks.py
@@ -1,9 +1,25 @@
 def initial_sticks(string):
     try:
-        if int(string) < 100:
+        if int(string) < 100 and int(string) > 10:
             return True
         else:
             return False
     except ValueError:
         return False
- 
+
+def player_choice(string):
+    try:
+        if int(string) in [1, 2, 3]:
+            return True
+        else: return False
+    except ValueError:
+        return False
+
+def sticks_left(sticks_left, last_choice):
+    return sticks_left - last_choice
+
+def game_over(sticks_left):
+    if sticks_left == 1:
+        return True
+    else:
+        return False

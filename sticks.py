e0972aaa5d0bc87a2c167d2ddd2098c665583dda
sateigen <shannon.teigen@gmail.com>
2016-07-19 14:03:35 -0400

    Game play set up for player vs. player mode.

--- sticks.py
+++ sticks.py
@@ -1,13 +1,13 @@
-def initial_sticks(string):
+def check_initial_sticks(string):
     try:
-        if int(string) < 100 and int(string) > 10:
+        if int(string) <= 100 and int(string) >= 10:
             return True
         else:
             return False
     except ValueError:
         return False
 
-def player_choice(string):
+def check_player_choice(string):
     try:
         if int(string) in [1, 2, 3]:
             return True
@@ -15,11 +15,55 @@ def player_choice(string):
     except ValueError:
         return False
 
-def sticks_left(sticks_left, last_choice):
+def reset_sticks(sticks_left, last_choice):
     return sticks_left - last_choice
 
-def game_over(sticks_left):
-    if sticks_left == 1:
+def is_game_over(sticks_left):
+    if sticks_left <= 1:
         return True
     else:
         return False
+
+
+def starting_player_vs_player():
+    print("How many sticks would you like your game to start with?")
+    while True:
+        starting_sticks = input("Choose a number between 10 and 100.\n\t>")
+        if check_initial_sticks(starting_sticks):
+            return int(starting_sticks)
+        elif not check_initial_sticks(starting_sticks):
+            print("That was not a valid choice.")
+            continue
+
+def get_user_choice(user_in):
+    while True:
+        if check_player_choice(user_in):
+            return int(user_in)
+        elif not check_player_choice(user_in):
+            print("That was not a valid choice.")
+            continue
+
+def reset_board(board):
+    if not is_game_over(board):
+        print("There are {} sticks left on the board.".format(board))
+    elif is_game_over(board):
+        print("You lost")
+
+
+def player_vs_player():
+    board = starting_player_vs_player()
+    while True:
+        player1_choice = get_user_choice(input("Player 1: How many sticks would you like to take away? (1, 2, 3)\n\t>"))
+        board -= player1_choice
+        reset_board(board)
+        if is_game_over(board):
+            break
+        player2_choice = get_user_choice(input("Player 2: How many sticks would you like to take away? (1, 2, 3)\n\t>"))
+        board -= player2_choice
+        reset_board(board)
+        if is_game_over(board):
+            break
+
+def main():
+    # if user wants to play against human:
+        player_vs_player
--- sticks_test.py
+++ sticks_test.py
@@ -5,9 +5,9 @@ def test_initial_amount_of_sticks_is_number_between_ten_and_one_hundred():
     number_of_sticks2 = 'g'
     number_of_sticks3 = '500'
 
-    assert initial_sticks(number_of_sticks) == True
-    assert initial_sticks(number_of_sticks2) == False
-    assert initial_sticks(number_of_sticks3) == False
+    assert check_initial_sticks(number_of_sticks) == True
+    assert check_initial_sticks(number_of_sticks2) == False
+    assert check_initial_sticks(number_of_sticks3) == False
 
 
 def test_player_choice_is_between_one_and_three():
@@ -15,19 +15,19 @@ def test_player_choice_is_between_one_and_three():
     number2 = '5'
     number3 = 's'
 
-    assert player_choice(number1) == True
-    assert player_choice(number2) == False
-    assert player_choice(number3) == False
+    assert check_player_choice(number1) == True
+    assert check_player_choice(number2) == False
+    assert check_player_choice(number3) == False
 
 def test_number_of_sticks_left():
     number_of_sticks = 100
     user_in = 2
 
-    assert sticks_left(number_of_sticks, user_in) == 98
+    assert reset_sticks(number_of_sticks, user_in) == 98
 
 def test_is_game_over_when_one_stick_left():
     sticks_left1 = 1
     sticks_left2 = 3
 
-    assert game_over(sticks_left1) == True
-    assert game_over(sticks_left2) == False
+    assert is_game_over(sticks_left1) == True
+    assert is_game_over(sticks_left2) == False
